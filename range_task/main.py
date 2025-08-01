6be9889426c8c7f7ee0483de9d0f17bb5a0f1497
VladimirRepetskiy <71264322+VladimirRepetskiy@users.noreply.github.com>
2025-08-01 20:03:32 +0700

    Initial commit

--- range_task/main.py
+++ range_task/main.py
@@ -1,2 +1,7 @@
 from range_task.range import Range
 
+diapason = Range(2.5, 7.8)
+print(f"Диапазон: от {diapason.start} до {diapason.end}")
+print(f"Длина диапазона: {diapason.length}")
+input_number = 5.0
+print(f"Число {input_number} внутри диапазона? {diapason.is_inside(input_number)}")
--- range_task/range.py
+++ range_task/range.py
@@ -1,5 +1,28 @@
 class Range:
 
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
+    def __init__(self, start:float, end:float):
+        self.__start = start
+        self.__end = end
+
+    @property
+    def start(self):
+        return self.__start
+
+    @start.setter
+    def start(self, start):
+        self.__start = start
+
+    @property
+    def end(self):
+        return self.__end
+
+    @end.setter
+    def end(self, end):
+        self.__end = end
+
+    @property
+    def length(self) -> float:
+        return self.__end - self.__start
+
+    def is_inside(self, number: float) -> bool:
+        return self.__start <= number <= self.__end
