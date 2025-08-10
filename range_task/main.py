f3bc3db002b07180fb42085da5d886d07c201b1c
VladimirRepetskiy <71264322+VladimirRepetskiy@users.noreply.github.com>
2025-08-10 13:05:15 +0700

    Initial commit

--- range_task/main.py
+++ range_task/main.py
@@ -1,7 +1,20 @@
 from range_task.range import Range
+import random
 
-diapason = Range(2.5, 7.8)
-print(f"Диапазон: от {diapason.start} до {diapason.end}")
-print(f"Длина диапазона: {diapason.length}")
+range = Range(2.5, 7.8)
+print(f"Диапазон: от {range.start} до {range.end}")
+print(f"Длина диапазона: {range.length}")
+
 input_number = 5.0
-print(f"Число {input_number} внутри диапазона? {diapason.is_inside(input_number)}")
+print(f"Число {input_number} внутри диапазона? {range.is_inside(input_number)}")
+
+a = random.randint(0, 100)
+b = random.randint(a, 100)
+c = random.randint(0, 100)
+d = random.randint(c, 100)
+range1 = Range(a, b)
+range2 = Range(c, d)
+print(f"Интервал пересечения, {(a, b)}, {c, d}: {Range.get_interval_intersection(range1, range2)}")
+print(f"Интервал объединения, {(a, b)}, {c, d}: {Range.get_interval_union(range1, range2)}")
+print(f"Разность интервалов, {(a, b)}, {c, d}: {Range.get_interval_difference(range1, range2)}")
+
--- range_task/range.py
+++ range_task/range.py
@@ -1,23 +1,22 @@
 class Range:
-
-    def __init__(self, start:float, end:float):
+    def __init__(self, start: float, end: float) -> None:
         self.__start = start
         self.__end = end
 
     @property
-    def start(self):
+    def start(self) -> float:
         return self.__start
 
     @start.setter
-    def start(self, start):
+    def start(self, start: float) -> None:
         self.__start = start
 
     @property
-    def end(self):
+    def end(self) -> float:
         return self.__end
 
     @end.setter
-    def end(self, end):
+    def end(self, end: float) -> None:
         self.__end = end
 
     @property
@@ -26,3 +25,21 @@ class Range:
 
     def is_inside(self, number: float) -> bool:
         return self.__start <= number <= self.__end
+
+    def get_interval_intersection(self, other):
+        if self.__end < other.__start or self.__start > other.__end:
+            return None
+        return [max(self.__start, other.__start), min(self.__end, other.__end)]
+
+    def get_interval_union(self, other):
+        if self.__end < other.__start or self.__start > other.__end:
+            return [(self.__start, self.__end), (other.__start, other.__end)]
+        return [min(self.__start, other.__start), max(self.__end, other.__end)]
+
+    def get_interval_difference(self, other):
+        if self.get_interval_intersection(other) is None:
+            return None
+        if self.start < other.start:
+            return [self.start, other.start]
+        if self.end > other.end:
+            return [other.end, self.end]
