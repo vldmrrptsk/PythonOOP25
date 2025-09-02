0b9777bf630a7192caeb0fe45575de334aaefdad
VladimirRepetskiy <71264322+VladimirRepetskiy@users.noreply.github.com>
2025-09-02 18:51:10 +0700

    Initial commit

--- range_task/main.py
+++ range_task/main.py
@@ -1,12 +1,12 @@
 from range_task.range import Range
 import random
 
-interval = Range(2.5, 7.8)
-print(f"Диапазон: от {interval.start} до {interval.end}")
-print(f"Длина диапазона: {interval.length}")
+range = Range(2.5, 7.8)
+print(f"Диапазон: от {range.start} до {range.end}")
+print(f"Длина диапазона: {range.length}")
 
 input_number = 5.0
-print(f"Число {input_number} внутри диапазона? {interval.is_inside(input_number)}")
+print(f"Число {input_number} внутри диапазона? {range.is_inside(input_number)}")
 
 left_border_1 = random.randint(0, 100)
 right_border_1 = random.randint(left_border_1, 100)
@@ -25,4 +25,4 @@ print(f"Объединение интервалов {left_border_1, right_border
       f"{interval_1.get_union(interval_2)}")
 print(f"Разность интервалов {left_border_1, right_border_1}, "
       f"{left_border_2, right_border_2}: "
-      f"{Range.get_difference(interval_1, interval_2)}")
+      f"{interval_1.get_difference(interval_2)}")
